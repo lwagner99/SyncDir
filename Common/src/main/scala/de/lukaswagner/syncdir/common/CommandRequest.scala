f7f522fc7cd1b9738faf0a96ac9d20ff6477d11f
Lukas <Lukas.Wagner@uni-duesseldorf.de>
2020-07-05 08:44:28 +0200

    removed useless parsing

--- Common/src/main/scala/de/lukaswagner/syncdir/common/CommandRequest.scala
+++ Common/src/main/scala/de/lukaswagner/syncdir/common/CommandRequest.scala
@@ -23,48 +23,18 @@ object SyncDirResponse {
 
 case class DownloadFileRequest(filePath: String, transmissionID: String) extends CommandRequest
 
-object DownloadFileRequest {
-  def apply(filePath: String, transmissionID: String): DownloadFileRequest = {
-    val parsedFileStreamPath = Utils.createOsIndependentStringPath(filePath)
-    new DownloadFileRequest(parsedFileStreamPath, transmissionID)
-  }
-}
 
 case class UploadFileRequest(filePath: String, transmissionID: String) extends CommandRequest
 
-object UploadFileRequest {
-  def apply(filePath: String, transmissionID: String): UploadFileRequest = {
-    val parsedFileStreamPath = Utils.createOsIndependentStringPath(filePath)
-    new UploadFileRequest(parsedFileStreamPath, transmissionID)
-  }
-}
 
 case class DeleteFileRequest(filePath: String, transmissionID: String) extends CommandRequest
 
-object DeleteFileRequest {
-  def apply(filePath: String, transmissionID: String): DeleteFileRequest = {
-    val parsedFileStreamPath = Utils.createOsIndependentStringPath(filePath)
-    new DeleteFileRequest(parsedFileStreamPath, transmissionID)
-  }
-}
 
 case class DownloadFileStream(transmissionID: String, filePath: String, sourceRef: SourceRef[ByteString]) extends CommandRequest
 
-object DownloadFileStream {
-  def apply(transmissionID: String, filePath: String, sourceRef: SourceRef[ByteString]): DownloadFileStream = {
-    val parsedFileStreamPath = Utils.createOsIndependentStringPath(filePath)
-    new DownloadFileStream(transmissionID, parsedFileStreamPath, sourceRef)
-  }
-}
 
 case class UploadFileStream(transmissionID: String, filePath: String, sourceRef: SourceRef[ByteString], clientCoordinatorActor: ActorRef) extends CommandRequest
 
-object UploadFileStream {
-  def apply(transmissionID: String, filePath: String, sourceRef: SourceRef[ByteString], clientCoordinatorActor: ActorRef): UploadFileStream = {
-    val parsedFileStreamPath = Utils.createOsIndependentStringPath(filePath)
-    new UploadFileStream(transmissionID, parsedFileStreamPath, sourceRef, clientCoordinatorActor)
-  }
-}
 
 case class TransmissionsAcknowledgement(transmissionID: String) extends CommandRequest
 
